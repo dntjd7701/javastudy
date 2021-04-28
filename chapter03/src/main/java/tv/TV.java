1d07560301427adf8be6bed584735fecff201c3f
dntjd7701 <71601453+dntjd7701@users.noreply.github.com>
2021-04-28 15:37:46 +0900

    과제 TV 정

---
+++ chapter03/src/main/java/tv/TV.java
@@ -0,0 +1,38 @@
+package tv;
+
+public class TV {
+	
+	private int channel; // 1~255
+	private int volume; // 0~100
+	private boolean power;
+	
+	// 조건
+	
+	
+	
+	
+	//setter, getter 만들지 말구 
+	
+	//true 면 켜지
+	public void power(boolean on) {
+		
+	}
+	
+	//true 면 채널 
+ 	public void channel(boolean up) {
+		
+	}
+ 	
+ 	public void volumn(boolean up) {
+ 		
+ 	}
+ 	
+ 	public void channel(int channel) {
+ 		
+ 	}
+
+	public void status() {
+		System.out.println("TV[power=on, channel=11, volumn=30]");
+	}
+
+}
