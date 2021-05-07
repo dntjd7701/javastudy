7f78936d8d6976e6692386df6e73e128296af179
dntjd7701 <71601453+dntjd7701@users.noreply.github.com>
2021-05-07 13:37:43 +0900

    UDP 호스트 주소 오류#1

--- network/src/main/java/udp/UDPTimeClient.java
+++ network/src/main/java/udp/UDPTimeClient.java
@@ -1,10 +1,66 @@
 package udp;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
 public class UDPTimeClient {
+	private static final String SERVER_IP = "127.0.0.1";
+	private static final int SERVER_PORT = 5312;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		DatagramSocket socket = null;
+		// 1. 소켓 생성 
+		try {
+			socket = new DatagramSocket();
+			
+			
+			// 2. 데이터 쓰기
+			
+			byte[] sendData = "".getBytes("UTF-8");
+			DatagramPacket sendPacket = new DatagramPacket(
+					sendData,
+					sendData.length,
+					new InetSocketAddress(SERVER_IP ,SERVER_PORT));
+			
+			// 3. 데이터 보내기
+			socket.send(sendPacket);
+			
+			// 4. 데이터 읽기 (날짜 데이터 받기)
+			
+			
+			DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
+			socket.receive(receivePacket);
+			
+			byte[] receiveData = receivePacket.getData();
+			int length = receivePacket.getLength();
+			String date = new String(receiveData, 0, length, "utf-8");
+			
+			// 5. 출력
+			
+			System.out.println("[받은 날짜] : " + date);
+			
+			
+			
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(socket != null && socket.isClosed() == false) {
+				socket.close();
+			}
+		}
+		
 	}
 
 }
--- network/src/main/java/udp/UDPTimeServer.java
+++ network/src/main/java/udp/UDPTimeServer.java
@@ -1,22 +1,51 @@
 package udp;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UDPTimeServer {
-
+	
 	public static void main(String[] args) {
-		//2. 데이터 수신 
-		DatagramPacket receivePacket = new DatagramPacket(new byte[0], 0);
-		socket.receive(receivePacket);
-		
-		
-		//3. 데이터 송신 
-		String now = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+		DatagramSocket socket = null;
 
-		// 클라이언트가 그냥 이렇게 보내면 돼 
-		byte[] sendData = "".getByte();
+		try {
+			// 1. 소켓 생성 
+			socket = new DatagramSocket(5312);
+			
+				// 2. 데이터 수신
+				DatagramPacket receivePacket = new DatagramPacket(new byte[0], 0);
+				socket.receive(receivePacket);
+				
+				
+
+				// 3. 데이터 쓰기 
+				String now = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+				byte[] sendData = now.getBytes("utf-8");
+				DatagramPacket sendPacket = new DatagramPacket(
+						sendData, 
+						sendData.length, 
+						receivePacket.getAddress(),
+						receivePacket.getPort());
+						
+						
+				// 4. 데이터 송신 
+				socket.send(sendPacket);
+				
+				
+				
+				
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(socket != null && socket.isClosed() == false) {
+				socket.close();
+			}
+		}
+
+
 	}
-
 }
