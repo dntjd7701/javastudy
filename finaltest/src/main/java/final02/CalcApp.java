503a4f67d33e3d646be1244d1bd2750980ebf41b
dntjd7701 <dntjd7701@gmail.com>
2021-05-15 15:29:54 +0900

    final02 success

--- finaltest/src/main/java/final02/CalcApp.java
+++ finaltest/src/main/java/final02/CalcApp.java
@@ -1,5 +1,6 @@
 
 package final02;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,36 +8,37 @@ import java.util.Scanner;
 public class CalcApp {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);		
+		Scanner scanner = new Scanner(System.in);
 		System.out.print("두 정수와 연산자를 입력하시오 >> ");
-		
-		//interface 변수 생성 및 초기
+
+		// interface 변수 생성 및 초기
 		Arithmetic ar = null;
 		// 두 정수의 입력값과 연산자 받
 		int left = scanner.nextInt();
 		int right = scanner.nextInt();
 		String s = scanner.next();
-		
-		//연산자 비
-		switch(s) {
+
+		// 연산자 비
+		switch (s) {
 		case "+":
-			
-			//각 연산자에 맞는 클래스 호출/메소드 호
+
+			// 각 연산자에 맞는 클래스 호출/메소드 호
 			ar = new Add();
 			System.out.println(ar.calculate(left, right));
 			break;
 		case "-":
-			ar= new Sub();
+			ar = new Sub();
 			System.out.println(ar.calculate(left, right));
 			break;
 		case "*":
+			ar = new Mul();
 			System.out.println(ar.calculate(left, right));
 			break;
 		case "/":
+			ar = new Div();
 			System.out.println(ar.calculate(left, right));
 			break;
 		}
 	}
-
 
 }
--- finaltest/src/main/java/final02/Mul.java
+++ finaltest/src/main/java/final02/Mul.java
@@ -4,7 +4,6 @@ public class Mul implements Arithmetic {
 
 	@Override
 	public int calculate(int a, int b) {
-		// TODO Auto-generated method stub
 		return a*b;
 	}
 
